688fd573afd921fec223bdacd896fcb40586aa35
khappucino <david.capino@gmail.com>
2019-11-12 21:38:47 -0800

    update the main.swift to pull the bootstrap out of the container, the boss and worker groups as well

--- Sources/TannhauserGate/MainAssembly.swift
+++ Sources/TannhauserGate/MainAssembly.swift
@@ -12,6 +12,29 @@ class MainAssembly {
         container.register(BasePipelineProvider.self) { r in
             BasePipelineProvider(clientChannelHandlerProvider: r.resolve(Provider<ClientChannelHandler>.self)!)
         }
+
+        container.register(MultiThreadedEventLoopGroup.self) { r, count in 
+            MultiThreadedEventLoopGroup(numberOfThreads: count) 
+        }
+
+        container.register(ServerBootstrap.self) { (r: Swinject.Resolver, bossGroup: MultiThreadedEventLoopGroup, workerGroup: MultiThreadedEventLoopGroup) -> ServerBootstrap in 
+            let basePipelineProvider: BasePipelineProvider = r.resolve(BasePipelineProvider.self)!
+            let bootstrap: ServerBootstrap = ServerBootstrap(group: bossGroup, childGroup: workerGroup)
+                // Specify backlog and enable SO_REUSEADDR for the server itself
+                .serverChannelOption(ChannelOptions.backlog, value: 256)
+                // Enable SO_REUSEADDR for the accepted Channels
+                .serverChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
+                // every time we get a new connection we will get a new pipeline initializer
+                .childChannelInitializer(basePipelineProvider.buildBasePipeline())
+                .childChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
+                .childChannelOption(ChannelOptions.maxMessagesPerRead, value: 16)
+                .childChannelOption(ChannelOptions.recvAllocator, value: AdaptiveRecvByteBufferAllocator())
+            return bootstrap
+        }
+
+
+
+
         return container
     }
 }
--- Sources/TannhauserGate/main.swift
+++ Sources/TannhauserGate/main.swift
@@ -4,24 +4,17 @@ import Swinject
 
 let container = MainAssembly().buildContainer()
 
-let basePipelineProvider = container.resolve(BasePipelineProvider.self)!
+let bossGroup: MultiThreadedEventLoopGroup = container.resolve(MultiThreadedEventLoopGroup.self, argument: 1)!
+let workerGroup: MultiThreadedEventLoopGroup = container.resolve(MultiThreadedEventLoopGroup.self, argument: 4)!
 
-let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
-let bootstrap = ServerBootstrap(group: group)
-    // Specify backlog and enable SO_REUSEADDR for the server itself
-    .serverChannelOption(ChannelOptions.backlog, value: 256)
-    .serverChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
-    // Enable SO_REUSEADDR for the accepted Channels
-    .childChannelInitializer(basePipelineProvider.buildBasePipeline())
-    .childChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
-    .childChannelOption(ChannelOptions.maxMessagesPerRead, value: 16)
-    .childChannelOption(ChannelOptions.recvAllocator, value: AdaptiveRecvByteBufferAllocator())
+let serverBootstrap = container.resolve(ServerBootstrap.self, arguments: bossGroup, workerGroup)!
+
 defer {
-    try! group.syncShutdownGracefully()
+    try! bossGroup.syncShutdownGracefully()
+    try! workerGroup.syncShutdownGracefully()
 }
 
-
-let channel = try bootstrap.bind(host: "127.0.0.1", port: 8080).wait()
+let channel = try serverBootstrap.bind(host: "127.0.0.1", port: 8080).wait()
 
 guard let localAddress = channel.localAddress else {
     fatalError("Address was unable to bind. Please check that the socket was not closed or that the address family was understood.")
